14df8359eb2d3fc041c19c1521e041baaff79e4e
nicky81818 <nicole.d.hsu@gmail.com>
2025-09-26 14:28:13 -0700

    add log

--- PartB/server.js
+++ PartB/server.js
@@ -1,10 +1,10 @@
-const http = require("http")
-const url = require("url")
-const fs = require("fs")
-const utils = require("./modules/utils")
-const message = require("./lang/en/en.json")
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const utils = require("./modules/utils");
+const message = require("./lang/en/en.json");
 const PORT = process.env.PORT || 8080;
-const HOST = "0.0.0.0"; 
+const HOST = "0.0.0.0";
 
 http.createServer(handleRequest).listen(PORT, HOST, () => {
   console.log(`Server is running on http://${HOST}:${PORT}`);
@@ -14,14 +14,19 @@ function handleRequest(req, res) {
   let q = url.parse(req.url, true);
   if (q.pathname === "/getDate/") {
     res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Access-Control-Allow-Origin': "*"
-    })
-    res.end(`<span style="color: blue;">${utils.getMessage(q.query.name)} ${utils.getFullDate()}</span>`);
+      "Content-Type": "text/html",
+      "Access-Control-Allow-Origin": "*",
+    });
+    res.end(
+      `<span style="color: blue;">${utils.getMessage(
+        q.query.name
+      )} ${utils.getFullDate()}</span>`
+    );
   } else if (q.pathname === "/writeFile/") {
     fs.appendFile("file.txt", q.query.text + "\n", (err) => {
       if (err) {
         console.error(err);
+        console.log("there was an error writing the file");
         return;
       }
       console.log("The file has been saved!");
@@ -38,7 +43,7 @@ function handleRequest(req, res) {
         res.writeHead(404, {
           "Content-Type": "text/json",
           "Access-Control-Allow-Origin": "*",
-        }); 
+        });
         res.end(`{"message": ${message.FILE_NOT_FOUND}}`);
         return console.log(err);
       }
@@ -50,5 +55,4 @@ function handleRequest(req, res) {
       res.end(`<div>${data}</div>`);
     });
   }
-
 }
